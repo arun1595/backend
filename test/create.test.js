10c152a654bde2a261c6cf888f3ae3874d64ea5d
Robert Jordan <robert@jordanjr.com>
2018-05-04 16:19:10 -0600

    Refactored tests for list and add to whitelist. create.test.js still needs some work.

--- test/create.test.js
+++ test/create.test.js
@@ -1,18 +1,28 @@
 const chai = require('chai');
-const expect = chai.expect;
+const expect = require('chai').expect;
 
-const server = process.env.BASE_URL;
+require('dotenv').config();
 
-describe('POST /whitelist', () => {
-    it('should return the submitted address', (done) => {
-      chai.request(server)
-      .post('/whitelist')
-      .send({address: '0x0047615d4f065c1973ab091fe55276d83b27773e'})
-      .end((err, res) => {
-        console.log(err)
-        expect(res.statusCode).to.equal(200);
-        expect(res.body.address);
-        done();
-      });
-    });
+const addToWhiteList = require('../whitelist/create');
+
+describe('Add whitelist address', () => {
+  it('should return the submitted address', async () => {
+    const callback = (error, result) => {
+      console.log(result);
+      expect(result).to.be.a('object');
+      expect(result).to.have.property('statusCode');
+      expect(result).to.have.property('body');
+      if (result.statusCode == 200) {
+        expect(result).to.have.property('headers');
+        expect(result).to.have.property('isBase64Encoded');
+        expect(result.body).to.be.a('string');
+        expect(result.isBase64Encoded).to.be.a('boolean');
+      } else if (result.statusCode == 400) {
+        expect(result.statusCode).to.equal(400); // placeholder
+      } else {
+        expect(result.statusCode).to.equal(502);
+      }
+    };
+    const result = await addToWhiteList.create({address: process.env.ADD_TO_WHITELIST_ADDRESS}, null, callback);
+  });
 });
--- test/list.test.js
+++ test/list.test.js
@@ -1,30 +1,26 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http')
-chai.use(chaiHttp);
-const expect = chai.expect;
+const expect = require('chai').expect;
 
-const server = process.env.BASE_URL;
+require('dotenv').config();
 
-describe('GET /whitelist', () => {
-    it('should return whitelist', (done) => {
-      chai.request(server)
-      .get('/whitelist')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        done();
-      });
-    });
-})
+const whiteList = require('../whitelist/list');
 
-describe('GET /dbentries', () => {
-    it('should return dbentries', (done) => {
-      chai.request(server)
-      .get('/dbentries')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        done();
-      });
-    });
-})
+describe('Get whitelist address', () => {
+  it('should return whitelist or check for a proper error object', async () => {
+    const callback = (error, result) => {
+      // console.log(result);
+      expect(result).to.be.a('object');
+      expect(result).to.have.property('statusCode');
+      expect(result).to.have.property('body');
+      if (result.statusCode == 200) {
+        expect(result).to.have.property('headers');
+        expect(result).to.have.property('isBase64Encoded');
+        expect(JSON.parse(result.body)).to.be.a('array');
+        expect(result.isBase64Encoded).to.be.a('boolean');
+      } else {
+        expect(result.statusCode).to.equal(502);
+      }
+    };
+    const result = await whiteList.getwhitelist(null, null, callback);
+  });
+});
